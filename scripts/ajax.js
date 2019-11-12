7edaac4e777b5741daf44fcf806153cda40ea922
Brandon-Watkins <54755499+Brandon-Watkins@users.noreply.github.com>
2019-11-11 23:46:55 -0700

    Up to step 5 of Part 2, assignment 8. Ajax.js isn't going to work atm.

---
+++ scripts/ajax.js
@@ -0,0 +1,21 @@
+﻿var xhr = new XMLHttpRequest();
+
+xhr.open("GET", "data/books.json", true);
+xhr.onload = getResults;
+
+var getResults = function () {
+    if (xhr.responseText) showResults(xhr.responseText.parse());
+}
+
+var showResults = function (jsObject) {
+    $.each("#books.temp").remove;
+    for (var i = 0; i < xhr.responseText.parse()) {
+        var temp = jsObject[i];// dont think this will work.
+        $("#books thead").append(temp);
+    }
+}
+
+$("#btnSearch").on("click", function () {
+    var textToFind = $("txtSearch").text;
+    if (textToFind !== "Search for...") xhr.send(textToFind);
+})
--- scripts/navbar.js
+++ scripts/navbar.js
@@ -11,5 +11,5 @@ $(document).ready(function () {
     $("body").prepend('<div class="navbar"><div class="drop-down"><button class="drop-down-list-button">Directory</button><div class="drop-down-list-items">'
         + '<a href="index.html">Assignments</a><a href="resume.html">Resume</a><a href="ass1form.html">Form</a><a href="csstest.html">CSS Test</a>'
         + '<a href="jsintro.html">JS Intro</a><a href="objects.html">Objects</a><a href="dom.html">DOM</a><a href="events.html">Events</a>'
-        + '<a href="jQuery.html">jQuery</a></div></div></div>');
+        + '<a href="jQuery.html">jQuery</a><a href="ajax.html">AJAX/JSON</a></div></div></div>');
 });
